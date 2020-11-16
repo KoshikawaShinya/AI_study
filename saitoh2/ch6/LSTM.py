02187daa21584a813e248a8d0532199b34900969
sin528 <satowimp0319@gmail.com>
2020-11-16 23:31:55 +0900

    2020/11/16

--- saitoh2/ch6/LSTM.py
+++ saitoh2/ch6/LSTM.py
@@ -35,3 +35,42 @@ class LSTM:
         # cとhについてゲートとの要素積を行い、データを通す割合を決める
         c_next = c_prev * forget_gate + g * input_gate
         h_next = np.tanh(c_next) * output_gate
+
+        self.cache = (x, h_prev, c_prev, input_gate, forget_gate, g, output_gate, c_next)
+        return h_next, c_next
+
+    def backward(self, dh_next, dc_next):
+        Wx, Wh, b = self.params
+        x, h_prev, c_prev, input_gate, forget_gate, g, output_gate, c_next = self.cache
+
+        tanh_c_next = np.tanh(c_next)
+
+        ds = dc_next + (dh_next * output_gate) * (1 - tanh_c_next ** 2)
+
+        dc_prev = ds * forget_gate
+
+        di = ds * g
+        df = ds * c_prev
+        do = dh_next * tanh_c_next
+        dg = ds * input_gate
+
+        di *= input_gate * (1 - input_gate)
+        df *= forget_gate * (1 - forget_gate)
+        do *= output_gate * (1 - output_gate)
+        dg *= (1 - g ** 2)
+
+        # sliceした各値を一つにまとめる
+        dA = np.hstack((df, dg, di, do))
+
+        dWh = np.dot(h_prev.T, dA)
+        dWx = np.dot(x.T, dA)
+        db = dA.sum(axis=0)
+
+        self.grads[0][...] = dWx
+        self.grads[1][...] = dWh
+        self.grads[2][...] = db
+
+        dx = np.dot(dA, Wx.T)
+        dh_prev = np.dot(dA, Wh.T)
+
+        return dx, dh_prev, dc_prev
