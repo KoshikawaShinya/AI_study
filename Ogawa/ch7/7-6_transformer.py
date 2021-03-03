e3ee17eab599a0f4c93fc445dfdafaf531fb2f29
sin528 <satowimp0319@gmail.com>
2021-03-03 12:06:44 +0900

    2021/03/03

--- Ogawa/ch7/7-6_transformer.py
+++ Ogawa/ch7/7-6_transformer.py
@@ -1,4 +1,5 @@
 from torch import nn
+from torch.nn import functional as F
 import torch
 import math
 
@@ -38,7 +39,7 @@ class PositionalEncoder(nn.Module):
                 pe[pos, i + 1] = math.cos(pos / (10000 ** ((2 * i) / d_model)))
 
         # 表peの先頭に。ミニバッチ次元となる次元を足す
-        self.pe = pe
+        self.pe = pe.unsqueeze(0)
 
         # 勾配を計算しないようにする
         self.pe.requires_grad = False
@@ -51,6 +52,98 @@ class PositionalEncoder(nn.Module):
         return ret
 
 
+class Attention(nn.Module):
+    '''Transformerは本当はマルチヘッドAttentionだが、分かりやすさを優先し、シングルAttentionで実装する'''
+
+    def __init__(self, d_model=300):
+        super().__init__()
+
+        # 1dConvのpointwise convolution ではなく、全結合層で特徴量を変換する
+        self.q_linear = nn.Linear(d_model, d_model)
+        self.v_linear = nn.Linear(d_model, d_model)
+        self.k_linear = nn.Linear(d_model, d_model)
+
+        # 出力時に使用する全結合層
+        self.out = nn.Linear(d_model, d_model)
+
+        # Attentionの大きさ調整の変数
+        self.d_k = d_model
+
+    def forward(self, q, k, v, mask):
+        # 全結合層で特徴量を変換
+        k = self.k_linear(k)
+        q = self.q_linear(q)
+        v = self.v_linear(v)
+
+        # Attentionの値を計算する
+        # 各値を足し算すると大きくなりすぎるので、root(d_k)で割って調整
+        weights = torch.matmul(q, k.transpose(1, 2)) / math.sqrt(self.d_k)
+
+        # ここでmaskを計算
+        mask = mask.unsqueeze(1)
+        weights = weights.masked_fill(mask==0, -1e9)
+
+        # softmaxで規格化をする
+        normalized_weights = F.softmax(weights, dim=-1)
+
+        # AttentionをValueと掛け算
+        output = torch.matmul(normalized_weights, v)
+
+        # 全結合層で特徴量を変換
+        output = self.out(output)
+
+        return output, normalized_weights
+
+class FeedForward(nn.Module):
+
+    def __init__(self, d_model, d_ff=1024, dropout=0.1):
+        '''Attention層から出力を単純に全結合層2つで特徴量を変換するだけのユニット'''
+        super().__init__()
+
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+
+        x = self.linear_1(x)
+        x = self.dropout(F.relu(x))
+        x = self.linear_2(x)
+        return x
+    
+class TransfomerBlock(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1):
+        super().__init__()
+
+        # LayerNormalization層
+        self.norm_1 = nn.LayerNorm(d_model)
+        self.norm_2 = nn.LayerNorm(d_model)
+
+        # Attention層
+        self.attn = Attention(d_model)
+
+        # Attention層
+        self.ff = FeedForward(d_model)
+
+        # Dropout
+        self.dropout_1 = nn.Dropout(dropout)
+        self.dropout_2 = nn.Dropout(dropout)
+    
+    def forward(self, x, mask):
+        # 正規化とAttention
+        x_normalized = self.norm_1(x)
+        output, normalized_weights = self.attn(x_normalized, x_normalized, x_normalized, mask)
+
+        x2 = x + self.dropout_1(output)
+
+        # 正規化と全結合層
+        x_normalized2 = self.norm_2(x2)
+        output = x2 + self.dropout_2(self.ff(x_normalized2))
+
+        return output, normalized_weights
+
+
 # 動作確認
 train_dl, val_dl, test_dl, TEXT = get_IMDb_DataLoaders_and_TEXT(max_length=256, batch_size=24)
 
@@ -60,12 +153,20 @@ batch = next(iter(train_dl))
 # モデル構築
 net1 = Embedder(TEXT.vocab.vectors)
 net2 = PositionalEncoder(d_model=300, max_seq_len=256)
+net3 = TransformerBlock(d_model=300)
+
+# maskの作成
+x = batch.TEXT[0]
+input_pad = 1   # 単語のIDにおいて、'<pad>' : 1 のため
+input_mask = (x != input_pad)
+print(input_mask[0])
 
 # 入出力
 x = batch.Text[0]
 x1 = net1(x)
 x2 = net2(x1)
+x3, normalized_weights = net3(x2, input_mask)   # Self-Attentionで特徴量を変換
 
-print('入力のテンソルサイズ : ', x1.shape)
-print('出力のテンソルサイズ : ', x2.shape)
-
+print('入力のテンソルサイズ : ', x2.shape)
+print('出力のテンソルサイズ : ', x3.shape)
+print('Attentionのサイズ : ', normalized_weights.shape)
